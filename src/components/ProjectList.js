758e029e158828542823021e199ab220c0c9899f
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-04-17 10:49:23 +0200

    hooks-lists-and-keys

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,24 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const renderProjects = projects.map((obj) => {
+    return (
+      <div key={obj.id}>
+        <ProjectItem
+          key={obj.id}
+          name={obj.name}
+          about={obj.about}
+          technologies={obj.technologies}
+        />
+      </div>
+    );
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{renderProjects}</div>
     </div>
   );
 }
